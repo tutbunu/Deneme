b17665fe7f4a8ab951e1f4057a600d2b37b817c3
Tutbunu <tuncayguven@gmail.com>
2019-06-18 16:44:51 +0200

    tutbunu

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var ejs = require('express-ejs-layouts');
+app.use(ejs);
 
-app.get('/', (req, res) => {
-	res.send('Merhaba Tutbunu!');
-});
+var bodyparser = require('body-parser'); // sunucudan gelen JSON datayı okumak için kullanılıyor
+var path = require('path');
+var fs = require('fs');
 
-app.get('/kategori', (req, res) => {
-	//res.send('Kategori sayfasındasınız!');
-	res.json({ 'product': 'Elma', 'price': 3.50, 'currency': 'TL' });
-});
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './models/views'));
+app.use(bodyparser.urlencoded({ extended:true }));
 
-app.get('/galeri/:categoryUrl/:titleUrl', (req, res) => {
-	const category = req.params.categoryUrl;
-	const titleUrl = req.params.titleUrl;
+app.use(session({ secret: '123456789', resave: false, saveUninitialized: true, cookie: { path: '/', httpOnly: true } }));//SESSİON KODU
 
-	res.send(`${category} kategorisindeki ${titleUrl} içeriğine bakıyorsunuz.`);
-});
+var router = require('./models/router/Router'); // Ana Omurgalardan
+app.use('/public', express.static(path.join(__dirname, 'public'))); //Public Klasörüne izin verme komutu.
+
+app.use('/', router); // ANA OMURGA
 
 app.listen(80, () => {
 	console.log('Uygulama çalıştırıldı...');
